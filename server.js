2511273514aa65fc756c910c9d1b9a9631b36fff
Kadika <dryan10101@gmail.com>
2022-06-15 14:00:26 -0700

    started writing inquirer menu and related function structure

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
-/* const mysql = require('mysql2'); */
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-/* const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'DarmanIsKadikasDad1138()',
-    database: 'company_db'
-  },
-  console.log(`Connected to the company_db database.`)
-); */
-
-app.get('/kadtest', (req, res) => {
-  res.send('Hello');
-})
 
 app.use(routes);
 
@@ -32,3 +18,40 @@ app.use((req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+//actual app runthrough
+const intro = ".--------------.\n|              |\n|   EMPLOYEE   |\n|   MANAGER    |\n|              |\n'--------------'";
+
+const menu = [
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'menuChoice',
+    choices: ['Add Employee', 'Add Role', 'Add Department', 'Update Employee Role', 'View All Employees', 'View All Roles', 'View All Departments'],
+  },
+];
+
+function addEmployee() {
+  console.log("adding employee");
+}
+
+function doChoice(choice) {
+  choice = choice.menuChoice;
+  if (choice = 'Add Employee') {addEmployee()};
+  /* if (choice = 'Add Role') {addRole()};
+  if (choice = 'Add Department') {addDepartment()};
+  if (choice = 'Update Employee Role') {updateEmployeeRole()};
+  if (choice = 'View All Employees') {viewEmployees()};
+  if (choice = 'View All Roles') {viewRoles()};
+  if (choice = 'View All Departments') {viewDepartments()}; */
+}
+
+function start() {
+  console.log(intro);
+  var begin = inquirer.prompt(menu);
+  begin.then((response) => {
+    doChoice(response);
+  });
+};
+
+start();
